af52fb24f00d6378e5e0601fa3ff98e7b026d36f
Naphatthason Na phatthalung <6810110474@psu.ac.th>
2025-12-17 18:39:47 +0700

    Finish Phase 2: Book Category Entity and Seeding

--- src/book-category/book-category.module.ts
+++ src/book-category/book-category.module.ts
@@ -3,7 +3,7 @@ import { BookCategoryService } from './book-category.service';
 import { BookCategoryController } from './book-category.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BookCategory } from './entities/book-category.entity';
-import { TypeOrmModule.forFeature[BookCategory]} from '@nestjs/typeorm';
+
 @Module({
   imports: [TypeOrmModule.forFeature([BookCategory])],
   controllers: [BookCategoryController],
